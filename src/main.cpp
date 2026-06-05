5fafef028da92e19e2951435c2e655d7f867a9b7
mike <262139264+plasmike@users.noreply.github.com>
2026-06-05 15:37:21 +0200

    feat: basic polyline

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <SFML/System/Vector2.hpp>
 #include <vector>
 
 #include <SFML/Graphics.hpp>
@@ -5,16 +6,31 @@
 int main() {
   sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML Spline Test");
 
+  // Points that should be connected in the spline
+  std::vector<sf::Vector2f> points = {
+    {100, 400}, {250, 100}, {400, 500}, {550, 200}
+  };
+
+  sf::VertexArray spline(sf::PrimitiveType::LineStrip);
+
+  for (sf::Vector2f point : points)
+  {
+    spline.append(sf::Vertex{{point}, sf::Color::Green});
+  }
+
   // run the program as long as the window is open
   while (window.isOpen())
   {
       // check all the window's events that were triggered since the last iteration of the loop
       while (const std::optional event = window.pollEvent())
       {
-          // "close requested" event: we close the window
           if (event->is<sf::Event::Closed>())
               window.close();
       }
+
+      window.clear();
+      window.draw(spline);
+      window.display();
   }
 
   return 0;
