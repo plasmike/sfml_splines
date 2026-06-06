55446aeb83a8f841c60db3b55b4f9bd6584b815a
mike <262139264+plasmike@users.noreply.github.com>
2026-06-06 11:32:50 +0200

    feat: draggable points

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Mouse.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Graphics.hpp>
 
@@ -24,17 +26,9 @@ int main() {
     {100, 400}, {250, 100}, {400, 500}, {450, 550}, {700, 400}
   };
 
-  sf::VertexArray spline(sf::PrimitiveType::LineStrip);
   int samplesPerSegment = 100;
-  for (int i = 0; i+3 < points.size(); i++)
-  {
-    for (int j = 0; j < samplesPerSegment; j++)
-    {
-      float t = j / static_cast<float>(samplesPerSegment);
-      sf::Vector2f pos = catmullRom(points[i], points[i+1], points[i+2], points[i+3], t);
-      spline.append(sf::Vertex{pos, sf::Color::Green});
-    }
-  }
+  float pointRadius = 5.f; // Radius for grabbing the points of the curve
+  int dragPointIndex = -1; // point being currently moved/dragged by the mouse | -1 = none
 
   // run the program as long as the window is open
   while (window.isOpen())
@@ -44,10 +38,53 @@ int main() {
       {
           if (event->is<sf::Event::Closed>())
               window.close();
+
+          if (const auto* evnt = event->getIf<sf::Event::MouseButtonPressed>())
+          {
+            sf::Vector2f mousePos = {static_cast<float>(evnt->position.x), static_cast<float>(evnt->position.y)};
+            for (int i = 0; i < points.size(); i++)
+            {
+              sf::Vector2f diff = mousePos - points[i];
+              if (sqrt(diff.x * diff.x + diff.y * diff.y) < pointRadius)
+              {
+                dragPointIndex = i;
+                break;
+              }
+            }
+          }
+
+          if (event->is<sf::Event::MouseButtonReleased>())
+            dragPointIndex = -1;
+
+          // Move point
+          if (const auto* evnt = event->getIf<sf::Event::MouseMoved>())
+          {
+            if (dragPointIndex != -1)
+              points[dragPointIndex] = {static_cast<float>(evnt->position.x), static_cast<float>(evnt->position.y)};
+          }
+      }
+
+      sf::VertexArray spline(sf::PrimitiveType::LineStrip);
+      for (int i = 0; i+3 < points.size(); i++)
+      {
+        for (int j = 0; j < samplesPerSegment; j++)
+        {
+          float t = j / static_cast<float>(samplesPerSegment);
+          sf::Vector2f pos = catmullRom(points[i], points[i+1], points[i+2], points[i+3], t);
+          spline.append(sf::Vertex{pos, sf::Color::Green});
+        }
       }
 
       window.clear();
       window.draw(spline);
+      for (sf::Vector2f point : points)
+      {
+        sf::CircleShape pointCircle(pointRadius);
+        pointCircle.setFillColor(sf::Color::Red);
+        pointCircle.setOrigin({pointRadius, pointRadius});
+        pointCircle.setPosition(point);
+        window.draw(pointCircle);
+      }
       window.display();
   }
 
